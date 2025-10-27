a6264315391551c43438af3b317de5e325d0ec8c
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-27 10:40:33 -0500

    feat(shared): add shared bounded context.

--- ACME.LearningCenterPlatform.API/Program.cs
+++ ACME.LearningCenterPlatform.API/Program.cs
@@ -9,10 +9,7 @@ builder.Services.AddOpenApi();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+if (app.Environment.IsDevelopment()) app.MapOpenApi();
 
 app.UseHttpsRedirection();
 
