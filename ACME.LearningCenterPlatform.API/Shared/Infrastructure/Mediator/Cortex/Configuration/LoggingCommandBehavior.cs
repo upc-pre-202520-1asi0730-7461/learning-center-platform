7098bfb140ca53c5df91052ddfffc7ad95972c64
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-29 10:00:45 -0500

    feat(logging): add logging-command-behavior to log command execution.

---
+++ ACME.LearningCenterPlatform.API/Shared/Infrastructure/Mediator/Cortex/Configuration/LoggingCommandBehavior.cs
@@ -0,0 +1,17 @@
+using Cortex.Mediator.Commands;
+
+namespace ACME.LearningCenterPlatform.API.Shared.Infrastructure.Mediator.Cortex.Configuration;
+
+/// <summary>
+/// Logs the execution of commands.
+/// </summary>
+/// <typeparam name="TCommand">The type of command.</typeparam>
+public class LoggingCommandBehavior<TCommand> : ICommandPipelineBehavior<TCommand> where TCommand : ICommand
+{
+    public async Task Handle(TCommand command, CommandHandlerDelegate next, CancellationToken cancellationToken)
+    {
+        // Log command start
+        Console.WriteLine($"Starting command: {typeof(TCommand).Name}");
+        await next();
+    }
+}
