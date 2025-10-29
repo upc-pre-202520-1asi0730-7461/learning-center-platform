bc67f132e6a0c93293c726e4576e659f946c7eac
Angel Velasquez <pcisavel@upc.edu.pe>
2025-10-29 09:56:19 -0500

    feat(event-handling): add event and event handler interfaces for mediator pattern integration.

---
+++ ACME.LearningCenterPlatform.API/Shared/Application/Internal/EventHandlers/IEventHandler.cs
@@ -0,0 +1,13 @@
+using ACME.LearningCenterPlatform.API.Shared.Domain.Model.Events;
+using Cortex.Mediator.Notifications;
+
+namespace ACME.LearningCenterPlatform.API.Shared.Application.Internal.EventHandlers;
+
+/// <summary>
+/// Represents a handler for a specific type of event.
+/// </summary>
+/// <typeparam name="TEvent">The type of event to handle.</typeparam>
+public interface IEventHandler<in TEvent> : INotificationHandler<TEvent> where TEvent : IEvent
+{
+    
+}
---
+++ ACME.LearningCenterPlatform.API/Shared/Domain/Model/Events/IEvent.cs
@@ -0,0 +1,13 @@
+using Cortex.Mediator.Notifications;
+
+namespace ACME.LearningCenterPlatform.API.Shared.Domain.Model.Events;
+
+/// <summary>
+/// Represents a domain event in the system.
+/// </summary>
+/// This interface extends <see cref="INotification"/>  to integrate with the mediator pattern for event handling.
+
+public interface IEvent : INotification
+{
+    
+}
