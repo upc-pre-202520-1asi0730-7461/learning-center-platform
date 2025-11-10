42409a46eb6c353b523448ea36c8dccb2f59b712
Angel Velasquez <pcisavel@upc.edu.pe>
2025-11-10 11:07:45 -0500

    feat(iam): implement authorization attributes and middleware for request handling.

---
+++ ACME.LearningCenterPlatform.API/IAM/Infrastructure/Pipeline/Middleware/Attributes/AllowAnonymousAttribute.cs
@@ -0,0 +1,10 @@
+namespace ACME.LearningCenterPlatform.API.IAM.Infrastructure.Pipeline.Middleware.Attributes;
+
+/**
+ * This attribute is used to decorate controllers and actions that do not require authorization.
+ * It skips authorization if the action is decorated with the [AllowAnonymous] attribute.
+ */
+[AttributeUsage(AttributeTargets.Method)]
+public class AllowAnonymousAttribute : Attribute
+{
+}
